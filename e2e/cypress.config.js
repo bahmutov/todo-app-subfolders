e8119a87abce47638e8a5b32c1d84a94d28da262
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-08-20 12:44:17 -0400

    turn on parallel run

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: '3wa1j2',
   e2e: {
     baseUrl: 'http://localhost:5555',
     // for such a simple project,
